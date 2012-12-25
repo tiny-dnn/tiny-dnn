53339b6b9c4108ca5d21927c3c8688b06135e2c7
Taiga Noumi <Noumi.Taiga@gmail.com>
2012-12-25 22:39:01 +0900

    implement connection table

--- include/convolutional_layer.h
+++ include/convolutional_layer.h
@@ -4,9 +4,24 @@
 
 namespace nn {
 
-template<int inc, int outc>
-struct convolutional_connection {
 
+struct connection_table {
+    connection_table() : rows_(0), cols_(0) {}
+    connection_table(const bool *ar, size_t rows, size_t cols) : connected_(rows * cols), rows_(rows), cols_(cols) {
+        std::copy(ar, ar + rows * cols, connected_.begin());
+    }
+
+    bool is_connected(int x, int y) const {
+        return is_empty() ? true : connected_[y * cols_ + x];
+    }
+
+    bool is_empty() const {
+        return rows_ == 0 && cols_ == 0;
+    }
+
+    std::vector<bool> connected_;
+    size_t rows_;
+    size_t cols_;
 };
 
 template<typename Activation>
@@ -20,16 +35,35 @@ public:
     weight_(window_size, window_size, in_channels*out_channels),
     window_size_(window_size)
     {
-        init_connection();
+        init_connection(connection_table());
+    }
+
+    convolutional_layer(int in_width, int in_height, int window_size, int in_channels, int out_channels, const connection_table& connection_table)
+        : partial_connected_layer<Activation>(in_width * in_height * in_channels, (in_width - window_size + 1) * (in_height - window_size + 1) * out_channels, 
+        window_size * window_size * in_channels * out_channels, out_channels), 
+        in_(in_width, in_height, in_channels), 
+        out_((in_width - window_size + 1), (in_height - window_size + 1), out_channels),
+        weight_(window_size, window_size, in_channels*out_channels),
+        window_size_(window_size)
+    {
+        init_connection(connection_table);
     }
 
 private:
-    void init_connection() {
-        for (int inc = 0; inc < in_.depth_; inc++)
-            for (int outc = 0; outc < out_.depth_; outc++)
+    void init_connection(const connection_table& table) {
+        int n = 0;
+        for (int inc = 0; inc < in_.depth_; inc++) {
+            for (int outc = 0; outc < out_.depth_; outc++) {
+                if (!table.is_connected(outc, inc)) {
+                    n++;
+                    continue;
+                }
+
                 for (int y = 0; y < out_.height_; y++)
                     for (int x = 0; x < out_.width_; x++)
                         connect_kernel(inc, outc, x, y);
+            }
+        }
 
         for (int outc = 0; outc < out_.depth_; outc++)
             for (int y = 0; y < out_.height_; y++)
