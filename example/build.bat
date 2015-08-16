fdb132a181bddb2efafa071b602c62d8c0f539d1
Taiga Noumi <Noumi.Taiga@gmail.com>
2015-08-16 20:54:51 +0900

    Fix compile error from build.bat

--- example/build.bat
+++ example/build.bat
@@ -1,1 +1,1 @@
-g++ -std=c++11 -Wall -Werror -Wextra -pedantic -I "C:/Program Files/boost/boost_1_55_0" -I ../include -O3 main.cpp
+g++ -std=c++11 -Wall -Werror -Wextra -pedantic -I "C:/Program Files/boost/boost_1_55_0" -I ../tiny_cnn -O3 main.cpp
