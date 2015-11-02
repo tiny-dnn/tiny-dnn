f55b29d0e4ca34156ae1bb61108c27039996dc11
Tony Di Croce <dicroce@gmail.com>
2015-11-02 13:50:40 -0800

    fix for signed / unsigned comparison error on GCC.

--- tiny_cnn/network.h
+++ tiny_cnn/network.h
@@ -633,11 +633,11 @@ std::vector<vec_t> image2vec(const float_t* data, const unsigned int  rows, cons
             //vec_t sample(sizepatch*sizepatch);
 
             if (i+sizepatch < cols && j+sizepatch < rows)
-            for (int k=0;k<sizepatch*sizepatch;k++)
+            for (unsigned int k=0;k<sizepatch*sizepatch;k++)
             //for_i(sizepatch*sizepatch, [&](int k)
             {
-                int y = k / sizepatch + j;
-                int x = k % sizepatch + i;
+                unsigned int y = k / sizepatch + j;
+                unsigned int x = k % sizepatch + i;
                 res[count][k] = data[x+y*cols];
             }
             //);
