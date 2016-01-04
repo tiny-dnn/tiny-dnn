e46a2d7232caa3bcc37a4c7171387c09dfd092a8
Taiga Noumi <Noumi.Taiga@gmail.com>
2016-01-04 18:56:08 +0900

    s/example/examples/

--- examples/main.cpp
+++ examples/main.cpp
@@ -26,7 +26,7 @@
 */
 #include <iostream>
 #include <memory>
-
+#define CNN_CAFFEINATED
 #include "tiny_cnn/tiny_cnn.h"
 //#define NOMINMAX
 //#include "imdebug.h"
@@ -38,10 +38,118 @@ void sample4_dropout();
 
 using namespace tiny_cnn;
 using namespace tiny_cnn::activation;
+using namespace std;
 
-int main(void) {
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+cv::Mat compute_mean(const string& mean_file, int width, int height)
+{
+    caffe::BlobProto mean_blob;
+    detail::read_proto_from_binary(mean_file, &mean_blob);
+
+    /* The format of the mean_blob file is planar 32-bit float BGR or grayscale. */
+    std::vector<cv::Mat> channels;
+    float* data = mean_blob.mutable_data()->mutable_data();
+    for (int i = 0; i < mean_blob.channels(); ++i) {
+        /* Extract an individual channel. */
+        cv::Mat channel(mean_blob.height(), mean_blob.width(), CV_32FC1, data);
+        channels.push_back(channel);
+        data += mean_blob.height() * mean_blob.width();
+    }
+
+    /* Merge the separate channels into a single image. */
+    cv::Mat mean;
+    cv::merge(channels, mean);
+
+    /* Compute the global mean pixel value and create a mean image
+    * filled with this value. */
+    cv::Scalar channel_mean = cv::mean(mean);
+    return cv::Mat(cv::Size(width, height), mean.type(), channel_mean);
+}
+
+cv::ColorConversionCodes get_cvt_codes(int src_channels, int dst_channels)
+{
+    assert(src_channels != dst_channels);
+
+    if (dst_channels == 3) 
+      return src_channels == 1 ? cv::COLOR_GRAY2BGR : cv::COLOR_BGRA2BGR;
+    else if (dst_channels == 1) 
+      return src_channels == 3 ? cv::COLOR_BGR2GRAY : cv::COLOR_BGRA2GRAY;
+    else
+      throw std::runtime_error("unsupported color code");
+}
+
+void preprocess(const cv::Mat& img, const cv::Mat& mean, int num_channels, cv::Size geometry, std::vector<cv::Mat>* input_channels)
+{
+    cv::Mat sample;
+
+    // convert color
+    if (img.channels() != num_channels)
+        cv::cvtColor(img, sample, get_cvt_codes(img.channels(), num_channels));
+    else
+        sample = img;
+
+    // resize
+    cv::Mat sample_resized;
+    cv::resize(sample, sample_resized, geometry);
+
+    cv::Mat sample_float;
+    sample_resized.convertTo(sample_float, num_channels == 3 ? CV_32FC3 : CV_32FC1);
+ 
+    // subtract mean
+    if (mean.size().width > 0) {
+        cv::Mat sample_normalized;
+        cv::subtract(sample_float, mean, sample_normalized);
+        cv::split(sample_normalized, *input_channels);
+    }
+    else {
+        cv::split(sample_float, *input_channels);
+    }
+}
+
+void test(const string& model_file, const string& trained_file, const string& mean_file, const string& label_file, const string& img_file)
+{
+    auto net = create_net_from_caffe_prototxt(model_file);
+    reload_weight_from_caffe_protobinary(trained_file, net.get());
+
+    int channels = net->in_shape().depth_;
+    int width = net->in_shape().width_;
+    int height = net->in_shape().height_;
+
+    cv::Mat img = cv::imread(img_file, -1);
+
+    auto mean = compute_mean(mean_file, width, height);
+
+    std::vector<float> inputvec(width*height*channels);
+    std::vector<cv::Mat> input_channels;
+    for (int i = 0; i < channels; i++) {
+        cv::Mat channel(height, width, CV_32FC1, &inputvec[width*height*i]);
+        input_channels.push_back(channel);
+    }
+
+    preprocess(img, mean, 3, cv::Size(width, height), &input_channels);
+
+    std::vector<double> vec(width*height*channels);
+    for (size_t i = 0; i < vec.size(); i++)
+        vec[i] = inputvec[i];
+    auto result = net->predict(vec);
+    //auto net = create_net_from_caffeproto("C:\\Users\\knowme\\Documents\\GitHub\\tiny-cnn\\vc_caffe\\caffe2tinycnn\\caffe2tinycnn\\deploy.prototxt");
+
+}
+
+int main(int argc, char** argv) {
+    int arg_channel = 1;
+    string model_file = argv[arg_channel++];
+    string trained_file = argv[arg_channel++];
+    string mean_file = argv[arg_channel++];
+    string label_file = argv[arg_channel++];
+    string img_file = argv[arg_channel++];
+
     try {
-    sample1_convnet();
+    test(model_file, trained_file, mean_file, label_file, img_file);
     }
     catch (const nn_error& e)
     {
