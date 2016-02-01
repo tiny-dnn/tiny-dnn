682d313f3a65192932199dac30cb5ac798e3d600
Aziz Baibabaev <aziz@sightcorp.com>
2016-02-02 00:11:49 +0100

    tmp

---
+++ .travis/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  brew update
+  [ -z $( brew tap | grep 'homebrew/science' ) ] && brew tap homebrew/science
+  [ -z "$( brew ls --versions tbb )" ] && brew install --c++11 tbb
+  [ -z "$( brew ls --versions boost )" ] && brew install --c++11 boost
+  [ -z "$( brew ls --versions cmake )" ] && brew install cmake
+  [ -z "$( brew ls --versions opencv3 )" ] && brew install -v --c++11 --with-tbb opencv3 && brew link --force opencv3 # verbose flag prevents the build from going stale
+elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  version_major=3
+  version_minor=1
+  version_patch=0  
+  OPENCV_DIR="$HOME/opencv_${version_major}_${version_minor}_${version_patch}/lib"
+  if [ ! -d  "$OPENCV_DIR" ]; then
+    git clone https://github.com/Itseez/opencv.git
+    cd opencv
+    git checkout ${version_major}.${version_minor}.${version_patch}
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$OPENCV_DIR" ..
+    make && make install
+  else
+    echo "Cached OpenCV ${1}.${2}.${3} found";
+  fi
+  
+  export LD_LIBRARY_PATH=${OPENCV_DIR}:$LD_LIBRARY_PATH
+
+  if [ "$CXX" == "g++" ]; then 
+    export CXX="g++-4.8" CC="gcc-4.8"
+  fi
+fi
