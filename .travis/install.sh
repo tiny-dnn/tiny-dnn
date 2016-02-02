9594b9cf9cf8c8baa5f1ed884825306fcf7ab05a
Aziz Baibabaev <aziz@sightcorp.com>
2016-02-02 17:03:33 +0100

    tmp

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,14 +6,15 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   [ -z $( brew tap | grep 'homebrew/science' ) ] && brew tap homebrew/science
   if [ -f "${HOME}/homebrew-cache/homebrew-cache.tar.gz"]; then
     tar -xvzf "${HOME}/homebrew-cache/homebrew-cache.tar.gz" --directory /usr/local/Cellar
-    brew link --force tbb boost cmake opencv3
+    brew link --force tbb boost cmake opencv3 jpeg libpng libtiff
+    brew install pkg-config
   else
     [ -z "$( brew ls --versions tbb )" ] && brew install --c++11 tbb
     [ -z "$( brew ls --versions boost )" ] && brew install --c++11 boost
     [ -z "$( brew ls --versions cmake )" ] && brew install cmake
-    [ -z "$( brew ls --versions opencv3 )" ] && brew install -v --c++11 --with-tbb opencv3 && brew link --force opencv3 # verbose flag prevents the build from going stale
+    [ -z "$( brew ls --versions opencv3 )" ] && brew install -v --c++11 --with-tbb --without-python --without-openexr --without-eigen --without-numpy opencv3 && brew link --force opencv3 # verbose flag prevents the build from going stale
     mkdir "${HOME}/homebrew-cache"
-    tar -czvf "${HOME}/homebrew-cache/homebrew-cache.tar.gz" --directory /usr/local/Cellar tbb boost cmake opencv3
+    tar -czvf "${HOME}/homebrew-cache/homebrew-cache.tar.gz" --directory /usr/local/Cellar tbb boost cmake opencv3 jpeg libpng libtiff
   fi
 elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
   version_major=3
