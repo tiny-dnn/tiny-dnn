917ec2914ebd87447d3d681215aae3573e8be9c0
Wangyida <wangyida123@outlook.com>
2016-06-16 22:23:04 +0800

    remove commited codes

--- examples/deconv/visual.cpp
+++ examples/deconv/visual.cpp
@@ -26,9 +26,6 @@
 */
 #include <iostream>
 #include <opencv2/opencv.hpp>
-/*#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>*/
 #include "tiny_cnn/tiny_cnn.h"
 
 using namespace tiny_cnn;
