d4ec7b66a91e2013d38bb744a68727be5408bcc0
edgarriba <edgar.riba@gmail.com>
2016-07-01 19:11:51 +0200

    generate CMake export configurations
    split summary to a cmake file
    
    split find dependencies
    
    split targets configuration
    
    split to CMakeLists.txt
    
    generate export configs
    
    fix travis issues
    
    add depenencies documentation
    
    set proper CMAKE_BUILD_TYPE
    
    force to use TBB OR OMP
    
    reorganize
    
    update TinyCNNConfig.cmake
    
    fix MSVC C++11 flag
    
    fix C++11 flag backward compatibility
    
    move OpenCV find_package to examples dir
    
    set minimum version to 3.0 since it's supported by last LTS
    
    simplify find_packages
    
    remove c++11 checking flag
    
    replace install dirs with project name
    
    update package export with CMake 3.x macros
    
    update docs configuration
    
    add small comment about CMake required version
    
    small fix with TINYCNN_VERSION
    
    update package config
    
    add POLICY CMP0048 checking
    
    partial working version
    
    clean non used variables
    
    fix package name
    
    fix PACKAGE_PREFIX_PATH, at least working: Black Magic!
    
    fix TravisCI cmake generation
    
    add DockerFile support
    
    fix broken examples linking
    
    add target_compile_options for compilation flags
    
    add config headers and reorganize Templates
    
    add NNPACK to Dockerfile and TravisCI
    
    fix compilation flags issue
    
    reorganize tests and examples
    
    add TinyCNN_USE_* to config file
    
    install phtread to Dockerfile
    
    fix WIN compilation flags errors
    
    fix -pthread linking error
    
    fix NNPACK tests for Dockerfile
    
    fix win test error
    
    test TravisCI again, including CMake 3.x
    
    enable trusty support
    
    update with lower_case style
    
    update lower_case style, TinyCNN_USE* options and uninstall command
    
    update TravisCI with OpenCV installation and matrix cases
    
    add NNPACK to TravisCI
    
    fix WIN include_install_dir_full
    
    fix use default cases in travis
    
    fix TravisCI errors
    
    add documentation

--- tiny_cnn/core/kernels/avx_kernel_common.h
+++ tiny_cnn/core/kernels/avx_kernel_common.h
@@ -35,13 +35,24 @@
         _mm256_insertf128_ps(_mm256_castps128_ps256(vb), va, 1)
 #endif
 
-inline __m256 madd(__m256 a, __m256 b, __m256 c) { return _mm256_add_ps(_mm256_mul_ps(a, b), c); }
-inline __m128 madd(__m128 a, __m128 b, __m128 c) { return _mm_add_ps(_mm_mul_ps(a, b), c); }
-inline __m128 madd_ss(__m128 a, __m128 b, __m128 c) { return _mm_add_ss(_mm_mul_ss(a, b), c); }
-
-inline __m256d madd(__m256d a, __m256d b, __m256d c) { return _mm256_add_pd(_mm256_mul_pd(a, b), c); }
-inline __m128d madd(__m128d a, __m128d b, __m128d c) { return _mm_add_pd(_mm_mul_pd(a, b), c); }
-inline __m128d madd_sd(__m128d a, __m128d b, __m128d c) { return _mm_add_sd(_mm_mul_sd(a, b), c); }
+inline __m256 madd(__m256 a, __m256 b, __m256 c) {
+    return _mm256_add_ps(_mm256_mul_ps(a, b), c);
+}
+inline __m128 madd(__m128 a, __m128 b, __m128 c) {
+    return _mm_add_ps(_mm_mul_ps(a, b), c);
+}
+inline __m128 madd_ss(__m128 a, __m128 b, __m128 c) {
+    return _mm_add_ss(_mm_mul_ss(a, b), c);
+}
+inline __m256d madd(__m256d a, __m256d b, __m256d c) {
+    return _mm256_add_pd(_mm256_mul_pd(a, b), c);
+}
+inline __m128d madd(__m128d a, __m128d b, __m128d c) {
+    return _mm_add_pd(_mm_mul_pd(a, b), c);
+}
+inline __m128d madd_sd(__m128d a, __m128d b, __m128d c) {
+    return _mm_add_sd(_mm_mul_sd(a, b), c);
+}
 
 // Horizontally add elements of __m256 type argument (sadly, _mm256_hadd_ps isn't good enough)
 // http://stackoverflow.com/a/13222410/4699324
