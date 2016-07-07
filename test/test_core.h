5cab9e930075dca5746c040dfbbec83b8e3d635f
edgarriba <edgar.riba@gmail.com>
2016-07-07 17:09:57 +0200

    add small interface to OpenCL

--- test/test_core.h
+++ test/test_core.h
@@ -36,7 +36,16 @@ namespace tiny_cnn {
 TEST(core, session) {
     core::session my_session(std::string("my_session"));
 
-    ASSERT_EQ(my_session.get_num_devices(), 0);
+    ASSERT_EQ(my_session.num_devices(), 0);
+    EXPECT_STREQ(my_session.name().c_str(), "my_session");
+
+    my_session.init_session();
+
+    EXPECT_TRUE(my_session.num_devices() != 0);
+
+    for (cnn_size_t i = 0; i < my_session.num_devices(); i++) {
+        my_session.print_device_info(i);
+    }
 }
 
 TEST(core, devices) {
@@ -51,74 +60,5 @@ TEST(core, backends) {
     // ASSERT_EQ(my_nnp_backend.get_context(), nullptr);
     // ASSERT_EQ(my_dnn_backend.get_context(), nullptr);
 }
-
-#ifdef CNN_HAVE_OPENCL
-TEST(core, opencl) {
-
-    char* value;
-    size_t valueSize;
-    cl_uint platformCount;
-    cl_platform_id* platforms;
-    cl_uint deviceCount;
-    cl_device_id* devices;
-    cl_uint maxComputeUnits;
- 
-    // get all platforms
-    clGetPlatformIDs(0, NULL, &platformCount);
-    platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * platformCount);
-    clGetPlatformIDs(platformCount, platforms, NULL);
-
-    EXPECT_TRUE(platformCount != 0);
-
-    for (int i = 0; i < (int)platformCount; i++) {
- 
-        // get all devices
-        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
-        devices = (cl_device_id*) malloc(sizeof(cl_device_id) * deviceCount);
-        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
- 
-        // for each device print critical attributes
-        for (int j = 0; j < (int)deviceCount; j++) {
- 
-            // print device name
-            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
-            value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
-            printf("%d. Device: %sn", j+1, value);
-            free(value);
- 
-            // print hardware device version
-            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
-            value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
-            printf(" %d.%d Hardware version: %sn", j+1, 1, value);
-            free(value);
- 
-            // print software driver version
-            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
-            value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
-            printf(" %d.%d Software version: %sn", j+1, 2, value);
-            free(value);
- 
-            // print c version supported by compiler for device
-            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
-            value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
-            printf(" %d.%d OpenCL C version: %sn", j+1, 3, value);
-            free(value);
- 
-            // print parallel compute units
-            clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS,
-                    sizeof(maxComputeUnits), &maxComputeUnits, NULL);
-            printf(" %d.%d Parallel compute units: %dn", j+1, 4, maxComputeUnits);
-        }
-        free(devices);
- 
-    }
-    free(platforms);
-
-}
-#endif
 
 } // namespace tiny-cnn
--- tiny_cnn/core/session.h
+++ tiny_cnn/core/session.h
@@ -29,6 +29,15 @@
 #include <string>
 #include <vector>
 
+#ifdef CNN_HAVE_OPENCL
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.hpp>
+#include <CL/opencl.h>
+#endif
+#endif
+
 #include "tiny_cnn/core/device.h"
 
 namespace tiny_cnn {
@@ -38,19 +47,87 @@ class session {
  public:
     session(const std::string name) : name_(name) {}
 
-    std::string get_name() const { return name_; }
-    size_t get_num_devices() const { return devices_.size(); }
-
     // will call construct graph
     // should we here specify the devices to use?
     void schedule_session(/* network<sequential>& net */);
 
     // will call forward or backward methods
     void run_session(/* data */);
+    
+    // Returns the session name
+    std::string name() const { return name_; }
+
+    // Returns the number of available devices
+    cnn_size_t num_devices() const { return devices_.size(); }
+
+    // Return the number of available platforms
+    cnn_size_t num_platforms() const { return platforms_.size(); }
+
+    void init_session() {
+#ifdef CNN_HAVE_OPENCL
+        // create platform
+        cl::Platform::get(&platforms_);
+        
+        // no found devices
+        if (platforms_.size() == 0) {
+            nn_warn("OpenCL cannot find any available device");
+            return;
+        }
+
+        // get all the available devices
+        // by default we are getting GPU fromfirst device
+        platforms_[0].getDevices(CL_DEVICE_TYPE_ALL, &devices_);
+
+        std::cout << "OpenCL has found " << num_devices() << " devices" << std::endl; 
+#else
+        nn_warn("TinyCNN has not been compiled with OpenCL support.");
+#endif
+    }
+
+#ifdef CNN_HAVE_OPENCL
+    // Returns an specified device
+    cl::Device get_device(const int id) {
+         if (id < 0 || id > static_cast<int>(num_devices())) {
+            nn_error("Cannot get device");
+        }
+        return devices_[id];    
+    }
+#endif
+
+    // Print the specified device info
+    void print_device_info(const int id) {
+#ifdef CNN_HAVE_OPENCL
+        cl::Device d = get_device(id);
+
+        std::cout << " ,--[ Device (" << id << ")  "          << "\n";
+        std::cout << " |`-> Hardware name:         "; 
+        std::cout << d.getInfo<CL_DEVICE_NAME>()              << "\n";
+        std::cout << " |`-> Hardware vendor:       "; 
+        std::cout << d.getInfo<CL_DEVICE_VENDOR>()            << "\n";
+        std::cout << " |`-> Hardware version:      "; 
+        std::cout << d.getInfo<CL_DEVICE_VERSION>()           << "\n";
+        std::cout << " |`-> Software version:      ";
+        std::cout << d.getInfo<CL_DRIVER_VERSION>()           << "\n";
+        std::cout << " |`-> OpenCL version:        ";
+        std::cout << d.getInfo<CL_DEVICE_OPENCL_C_VERSION>()  << "\n";
+        std::cout << "  `-> Parallel compute unit: ";
+        std::cout << d.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << "\n\n";
+#else
+        nn_warn("TinyCNN has not been compiled with OpenCL support.");
+#endif
+    }
 
  private:
-  std::string name_;
-  std::vector<std::shared_ptr<tiny_cnn::core::device>> devices_;
+    std::string name_;
+ 
+#ifdef CNN_HAVE_OPENCL
+    std::vector<cl::Device> devices_;
+    std::vector<cl::Platform> platforms_;
+#else
+    // TODO: Just for quick test
+    std::vector<int> platforms_;
+    std::vector<std::shared_ptr<tiny_cnn::core::device>> devices_;
+#endif
 };
 
 }  // namespace core
