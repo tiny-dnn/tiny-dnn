f02dc21452a22b98a2bbdcc9a9be454d3185ebf7
Kai Wolf <kai.wolf@gmail.com>
2016-10-25 19:32:43 +0200

    Normalize license header in tiny-dnn source files

--- tiny_dnn/util/target_cost.h
+++ tiny_dnn/util/target_cost.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2013-2016, Taiga Nomi. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #pragma once
 #include "tiny_dnn/util/util.h"
 #include <numeric> // std::accumulate
