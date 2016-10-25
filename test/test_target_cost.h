3be73824c901dedcb60e856ccd74d4320b9ce5ba
Kai Wolf <kai.wolf@gmail.com>
2016-10-25 19:19:56 +0200

    Normalize license headers in test files

--- test/test_target_cost.h
+++ test/test_target_cost.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2013-2016, Taiga Nomi. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #pragma once
 #include "picotest/picotest.h"
 #include "testhelper.h"
