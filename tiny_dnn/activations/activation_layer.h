13b8ea03dd3ff0457be96961a9a5b4bc94006e7e
Karan Desai <karandesai_96@live.com>
2017-01-29 14:38:45 +0530

    Reformat code, move tight for loops into virtual functions.
    - Calling virtual functions from inside a tight 'for' loop
      hurts performance.
    
    - forward_activation and backward_activation will now take
      a vector of float_t (vec_t) and iterate through it.

--- tiny_dnn/activations/activation_layer.h
+++ tiny_dnn/activations/activation_layer.h
@@ -6,62 +6,51 @@
 namespace tiny_dnn {
 
 class activation_layer : public layer {
-public:
-    /**
-     * @param in_shape [in] shape of input tensor
-     */
-    activation_layer(const shape3d &in_shape)
-            : layer({vector_type::data}, {vector_type::data}),
-              in_shape_(in_shape) {}
+ public:
+  /**
+   * @param in_shape [in] shape of input tensor
+   */
+  activation_layer(const shape3d &in_shape)
+    : layer({vector_type::data}, {vector_type::data}), in_shape_(in_shape) {}
 
-    std::vector<shape3d> in_shape() const override {
-        return {in_shape_};
+  std::vector<shape3d> in_shape() const override { return {in_shape_}; }
+
+  std::vector<shape3d> out_shape() const override { return {in_shape_}; }
+
+  void forward_propagation(const std::vector<tensor_t *> &in_data,
+                           std::vector<tensor_t *> &out_data) {
+    const tensor_t &x = *in_data[0];
+    tensor_t &y       = *out_data[0];
+
+    for (serial_size_t i = 0; i < x.size(); i++) {
+      forward_activation(x[i], y[i]);
     }
+  }
 
-    std::vector<shape3d> out_shape() const override {
-        return {in_shape_};
+  void back_propagation(const std::vector<tensor_t *> &in_data,
+                        const std::vector<tensor_t *> &out_data,
+                        std::vector<tensor_t *> &out_grad,
+                        std::vector<tensor_t *> &in_grad) override {
+    tensor_t &dx       = *in_grad[0];
+    const tensor_t &dy = *out_grad[0];
+    const tensor_t &x  = *in_data[0];
+    const tensor_t &y  = *out_data[0];
+
+    for (serial_size_t i = 0; i < x.size(); i++) {
+      backward_activation(x[i], y[i], dx[i], dy[i]);
     }
+  }
 
-    void forward_propagation(const std::vector<tensor_t *> &in_data,
-                             std::vector<tensor_t *> &out_data) {
-        const tensor_t &x = *in_data[0];
-        tensor_t &y       = *out_data[0];
+  virtual std::string layer_type() const = 0;
 
-        for (serial_size_t i = 0; i < x.size(); i++) {
-            for (serial_size_t j = 0; j < x[i].size(); j++) {
-                forward_activation(x[i][j], y[i][j]);
-            }
-        }
-    }
+  virtual void forward_activation(const vec_t &x, vec_t &y) = 0;
 
-    void back_propagation(const std::vector<tensor_t *> &in_data,
-                          const std::vector<tensor_t *> &out_data,
-                          std::vector<tensor_t *> &out_grad,
-                          std::vector<tensor_t *> &in_grad) override {
-        tensor_t &dx       = *in_grad[0];
-        const tensor_t &dy = *out_grad[0];
-        const tensor_t &x  = *in_data[0];
-        const tensor_t &y  = *out_data[0];
+  virtual void backward_activation(const vec_t x,
+                                   const vec_t &y,
+                                   vec_t &dx,
+                                   const vec_t &dy) = 0;
 
-        for (serial_size_t i = 0; i < x.size(); i++) {
-            for (serial_size_t j = 0; j < x[i].size(); j++) {
-                backward_activation(x[i][j], y[i][j], dx[i][j], dy[i][j]);
-            }
-        }
-    }
-
-    virtual std::string layer_type() const = 0;
-
-    virtual void forward_activation(const float_t &x,
-                                    float_t &y) = 0;
-
-    virtual void backward_activation(const float_t x,
-                                     const float_t &y,
-                                     float_t &dx,
-                                     const float_t &dy) = 0;
-
-private:
-    shape3d in_shape_;
-
+ private:
+  shape3d in_shape_;
 };
 }  // namespace tiny_dnn
--- tiny_dnn/activations/relu_layer.h
+++ tiny_dnn/activations/relu_layer.h
@@ -1,25 +1,29 @@
 #pragma once
-#include "tiny_dnn/layers/layer.h"
 #include "tiny_dnn/activations/activation_layer.h"
+#include "tiny_dnn/layers/layer.h"
 
 namespace tiny_dnn {
 
 class relu_layer : public activation_layer {
-public:
-    using activation_layer::activation_layer;
+ public:
+  using activation_layer::activation_layer;
 
-    std::string layer_type() const override {
-        return "relu-activation";
-    }
+  std::string layer_type() const override { return "relu-activation"; }
 
-    void forward_activation(const float_t &x, float_t &y) {
-        y = std::max(float_t(0), x);
+  void forward_activation(const vec_t &x, vec_t &y) {
+    for (serial_size_t j = 0; j < x.size(); j++) {
+      y[j] = std::max(float_t(0), x[j]);
     }
+  }
 
-    void backward_activation(const float_t x, const float_t &y,
-                             float_t &dx, const float_t &dy) {
-        float_t relu_grad = y > float_t(0) ? float_t(1) : float_t(0);
-        dx = dy * relu_grad;
+  void backward_activation(const vec_t x,
+                           const vec_t &y,
+                           vec_t &dx,
+                           const vec_t &dy) {
+    for (serial_size_t j = 0; j < x.size(); j++) {
+      float_t relu_grad = y[j] > float_t(0) ? float_t(1) : float_t(0);
+      dx[j]             = dy[j] * relu_grad;
     }
+  }
 };
 }  // namespace tiny_dnn
