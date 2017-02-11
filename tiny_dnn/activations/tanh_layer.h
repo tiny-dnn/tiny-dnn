237f4c7cc0f3239706285001698f8464b1496ab3
Karan Desai <karandesai_96@live.com>
2017-02-11 13:34:54 +0530

    Retain only Taiga Nomi's name in LICENSE comments of new files.

--- tiny_dnn/activations/tanh_layer.h
+++ tiny_dnn/activations/tanh_layer.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2017, Taiga Nomi, Karan Desai
+    Copyright (c) 2017, Taiga Nomi
     All rights reserved.
 
     Use of this source code is governed by a BSD-style license that can be found
