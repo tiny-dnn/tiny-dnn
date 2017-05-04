7faab83780b2306cf09a43fab202f4b59445d987
Evgenii <eabesea@rambler.ru>
2017-05-04 16:39:09 +0300

    Change parameters to xarray

--- tiny_dnn/core/kernels/fully_connected_op_internal.h
+++ tiny_dnn/core/kernels/fully_connected_op_internal.h
@@ -7,20 +7,24 @@
 */
 #pragma once
 
+#include "tiny_dnn/xtensor/xarray.hpp"
+#include "tiny_dnn/xtensor/xview.hpp"
+
 #include "tiny_dnn/core/params/fully_params.h"
 
 namespace tiny_dnn {
 namespace kernels {
 
-inline void fully_connected_op_internal(const tensor_t &in_data,
-                                        const vec_t &W,
-                                        const vec_t &bias,
-                                        tensor_t &out_data,
+inline void fully_connected_op_internal(const xt::xarray<float_t> &in_data,
+                                        const xt::xarray<float_t> &W,
+                                        const xt::xarray<float_t> &bias,
+                                        xt::xarray<float_t> &out_data,
                                         const fully_params &params,
                                         const bool layer_parallelize) {
   for_i(layer_parallelize, in_data.size(), [&](int sample) {
-    const vec_t &in = in_data[sample];
-    vec_t &out      = out_data[sample];
+
+    auto const in = xt::view(in_data, sample, xt::all());
+    auto out = xt::view(out_data, sample, xt::all());
 
     for (serial_size_t i = 0; i < params.out_size_; i++) {
       out[i] = float_t{0};
@@ -35,20 +39,20 @@ inline void fully_connected_op_internal(const tensor_t &in_data,
   });
 }
 
-inline void fully_connected_op_internal(const tensor_t &prev_out,
-                                        const vec_t &W,
-                                        tensor_t &dW,
-                                        tensor_t &db,
-                                        tensor_t &curr_delta,
-                                        tensor_t &prev_delta,
+inline void fully_connected_op_internal(const xt::xarray<float_t> &prev_out,
+                                        const xt::xarray<float_t> &W,
+                                        xt::xarray<float_t> &dW,
+                                        xt::xarray<float_t> &db,
+                                        xt::xarray<float_t> &curr_delta,
+                                        xt::xarray<float_t> &prev_delta,
                                         const fully_params &params,
                                         const bool layer_parallelize) {
   for (serial_size_t sample = 0; sample < prev_out.size(); sample++) {
     for (serial_size_t c = 0; c < params.in_size_; c++) {
       // propagate delta to previous layer
       // prev_delta[c] += current_delta[r] * W_[c * out_size_ + r]
-      prev_delta[sample][c] += vectorize::dot(
-        &curr_delta[sample][0], &W[c * params.out_size_], params.out_size_);
+      prev_delta(sample,c) += vectorize::dot(
+        &curr_delta(sample,0), &W[c * params.out_size_], params.out_size_);
     }
 
     for_(layer_parallelize, 0, size_t(params.out_size_),
@@ -56,15 +60,15 @@ inline void fully_connected_op_internal(const tensor_t &prev_out,
            // accumulate weight-step using delta
            // dW[c * out_size + i] += current_delta[i] * prev_out[c]
            for (serial_size_t c = 0; c < params.in_size_; c++) {
-             vectorize::muladd(&curr_delta[sample][r.begin()],
-                               prev_out[sample][c], r.end() - r.begin(),
-                               &dW[sample][c * params.out_size_ + r.begin()]);
+             vectorize::muladd(&curr_delta(sample,r.begin()),
+                               prev_out(sample,c), r.end() - r.begin(),
+                               &dW(sample,c * params.out_size_ + r.begin()));
            }
 
            if (params.has_bias_) {
              // vec_t& db = *in_grad[2];
              for (size_t i = r.begin(); i < r.end(); i++) {
-               db[sample][i] += curr_delta[sample][i];
+               db(sample,i) += curr_delta(sample,i);
              }
            }
          });
