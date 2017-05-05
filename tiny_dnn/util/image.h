3a2a222734a60de1fcb326261286daf2dfa6f372
beru <berupon@gmail.com>
2017-05-05 13:07:17 +0930

    Update third_party/stb (#685)
    * update third_party/stb
    
    * static inline

--- tiny_dnn/util/image.h
+++ tiny_dnn/util/image.h
@@ -21,14 +21,17 @@
 #endif
 
 #define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_INLINE  // We need this define to avoid multiple definition
+#define STB_IMAGE_STATIC
+#define STB_IMAGE_INLINE
 #include "third_party/stb/stb_image.h"
 
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_STATIC
 #define STB_IMAGE_RESIZE_INLINE
 #include "third_party/stb/stb_image_resize.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_STATIC
 #define STB_IMAGE_WRITE_INLINE
 #include "third_party/stb/stb_image_write.h"
 
