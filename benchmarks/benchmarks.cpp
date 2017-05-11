f4942b17bb8cbb7f02dbf0d75ff651035af33f73
Karan Desai <karandesai_96@live.com>
2017-05-11 06:04:22 +0530

    Add google benchmark framework.

---
+++ benchmarks/benchmarks.cpp
@@ -0,0 +1,10 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include "benchmark/benchmark.h"
+#include "tiny_dnn/tiny_dnn.h"
+
+#include "bm_global_avepool.h"
+using namespace tiny_dnn::benchmarks;
+
+BENCHMARK_MAIN();
