27fee0a0b953b0197dc19ade33adc6c99226f5d0
sid <srk97c@gmail.com>
2017-06-13 23:05:51 +0530

    Applied clang-format

--- tiny_dnn/activations/selu_layer.h
+++ tiny_dnn/activations/selu_layer.h
@@ -9,66 +9,90 @@
 #include "tiny_dnn/activations/activation_layer.h"
 #include "tiny_dnn/layers/layer.h"
 
-namespace tiny_dnn{
+namespace tiny_dnn {
 
-	class selu_layer : public activation_layer{
-	public:
-		
-		selu_layer(const float_t lambda=1.05070,const float_t alpha=1.67326)
-		: selu_layer(shape3d(0,0,0),lambda,alpha){}
+class selu_layer : public activation_layer {
+ public:
+  /**
+   * Construct a selu which will take shape when connected to some
+   * layer. Connection happens like ( layer1 << act_layer1 ) and shape of this
+   * layer is inferred at that time.
+   */
 
-		selu_layer(serial_size_t in_dim,const float_t lambda=1.05070,const float_t alpha=1.67326)
-		: selu_layer(shape3d(in_dim,1,1),lambda,alpha){}
+  selu_layer(const float_t lambda = 1.05070, const float_t alpha = 1.67326)
+      : selu_layer(shape3d(0, 0, 0), lambda, alpha) {}
 
-		selu_layer(serial_size_t in_width,
-				   serial_size_t in_height,
-				   serial_size_t in_channels,
-				   const float_t lambda=1.05070,
-				   const float_t alpha=1.67326)
-		: selu_layer(shape3d(in_width,in_height,in_channels),lambda,alpha){}
+  /**
+   * Construct a flat selu with specified number of neurons.
+   * This constructor is suitable for adding an activation layer after
+   * flat layers such as fully connected layers.
+   *
+   * @param in_dim      [in] number of elements of the input
+   */
 
-		selu_layer(const shape3d &in_shape,const float_t lambda=1.05070,const float_t alpha=1.67326)
-		: activation_layer(in_shape),lambda_(lambda),alpha_(alpha){}
+  selu_layer(serial_size_t in_dim, const float_t lambda = 1.05070,
+             const float_t alpha = 1.67326)
+      : selu_layer(shape3d(in_dim, 1, 1), lambda, alpha) {}
 
-		selu_layer(const layer &prev_layer,const float_t lambda=1.05070,const float_t alpha=1.67326)
-		: activation_layer(prev_layer),lambda_(lambda),alpha_(alpha){}
+  /**
+   * Construct a selu with specified width, height and channels.
+   * This constructor is suitable for adding an activation layer after spatial
+   * layers such as convolution / pooling layers.
+   *
+   * @param in_width    [in] number of input elements along width
+   * @param in_height   [in] number of input elements along height
+   * @param in_channels [in] number of channels (input elements along depth)
+   */
 
+  selu_layer(serial_size_t in_width, serial_size_t in_height,
+             serial_size_t in_channels, const float_t lambda = 1.05070,
+             const float_t alpha = 1.67326)
+      : selu_layer(shape3d(in_width, in_height, in_channels), lambda, alpha) {}
 
-		std::string layer_type() const override {
-			return "selu-activation";
-		}
+  /**
+   * Construct a selu layer with specified input shape.
+   *
+   * @param in_shape [in] shape of input tensor
+   */
 
-		float_t lambda_value(){return lambda_;}
+  selu_layer(const shape3d &in_shape, const float_t lambda = 1.05070,
+             const float_t alpha = 1.67326)
+      : activation_layer(in_shape), lambda_(lambda), alpha_(alpha) {}
 
-		float_t alpha_value(){return alpha_;}
+  selu_layer(const layer &prev_layer, const float_t lambda = 1.05070,
+             const float_t alpha = 1.67326)
+      : activation_layer(prev_layer), lambda_(lambda), alpha_(alpha) {}
 
-		void forward_activation(const vec_t &x,vec_t &y) override{
-			for (serial_size_t j = 0; j < x.size(); j++) {
-				y[j] = lambda_ * (x[j] > float_t(0) ? x[j] : alpha_*(std::exp(x[j])-float_t(1)) );
-			}
-		}
+  std::string layer_type() const override { return "selu-activation"; }
 
-		void backward_activation(const vec_t &x,
-								 const vec_t &y,
-								 vec_t &dx,
-								 const vec_t &dy) override{
-			// dx = dy * (gradient of selu)
-			for(serial_size_t j=0; j<x.size();j++){
-				dx[j] = dy[j] * (y[j] > float_t(0) ? lambda_ : (y[j] + lambda_*alpha_));
+  float_t lambda_value() { return lambda_; }
 
-			}
-		}
+  float_t alpha_value() { return alpha_; }
 
-		std::pair<float_t, float_t> scale() const override {
-    		return std::make_pair(float_t(0.1), float_t(0.9));
-  		}
+  void forward_activation(const vec_t &x, vec_t &y) override {
+    for (serial_size_t j = 0; j < x.size(); j++) {
+      y[j] =
+          lambda_ *
+          (x[j] > float_t(0) ? x[j] : alpha_ * (std::exp(x[j]) - float_t(1)));
+    }
+  }
 
-  		float_t lambda_;
-  		float_t alpha_;
+  void backward_activation(const vec_t &x, const vec_t &y, vec_t &dx,
+                           const vec_t &dy) override {
+    // dx = dy * (gradient of selu)
+    for (serial_size_t j = 0; j < x.size(); j++) {
+      dx[j] = dy[j] * (y[j] > float_t(0) ? lambda_ : (y[j] + lambda_ * alpha_));
+    }
+  }
 
-  		friend struct serialization_buddy;
+  std::pair<float_t, float_t> scale() const override {
+    return std::make_pair(float_t(0.1), float_t(0.9));
+  }
 
+  float_t lambda_;
+  float_t alpha_;
 
-	};
-	
-} //namespace tiny_dnn
+  friend struct serialization_buddy;
+};
+
+}  // namespace tiny_dnn
