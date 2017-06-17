7c10e52f64269f13743b6e8bf33e9f7cc807853c
sid <srk97c@gmail.com>
2017-06-17 23:57:09 +0530

    Add gradient check test for selu layer

---
+++ test/test_selu_layer.h
@@ -0,0 +1,18 @@
+//#pragma once
+#include "gtest/gtest.h"
+#include "testhelper.h"
+#include "tiny_dnn/tiny_dnn.h"
+
+using namespace tiny_dnn::activation;
+
+namespace tiny_dnn {
+TEST(selu, gradient_check) {
+  network<sequential> nn;
+  nn << selu((serial_size_t)3, (serial_size_t)3, (serial_size_t)1);
+
+  const auto test_data = generate_gradient_check_data(nn.in_data_size());
+  nn.init_weight();
+  EXPECT_TRUE(nn.gradient_check<mse>(test_data.first, test_data.second,
+                                     epsilon<float_t>(), GRAD_CHECK_ALL));
+}
+}
