0e79d258c6d6c6168e4d2aa28b8ec9badf7f019d
EdgAR <edgar.riba@gmail.com>
2017-06-25 12:13:44 -0700

    threadpool as a standalone class

--- tiny_dnn/core/kernels/fully_connected_op_nnpack.h
+++ tiny_dnn/core/kernels/fully_connected_op_nnpack.h
@@ -14,6 +14,7 @@
 
 namespace tiny_dnn {
 namespace kernels {
+
 /**
  * Forward propogation for fully connected layer with NNPACK backend
  * @param in_data
@@ -37,30 +38,27 @@ inline void fully_connected_op_nnpack(const Tensor<float, S1> &in_data,
   const float *input_ptr  = in_data.host_pointer(0, 0);
   float *output_ptr       = out_data.host_pointer(0, 0);
 
-  // TODO: embed it into a class
-  const size_t num_mkl_threads = std::thread::hardware_concurrency();
-  pthreadpool_t threadpool     = pthreadpool_create(num_mkl_threads);
-
-	// TODO(edgarriba): why is it index `[1]`? recheck once parameters refactor is in. We'll assume here NCHW ?
-  size_t in_size = in_data.shape()[1];
+  // TODO(edgarriba): why is it index `[1]`? recheck once parameters refactor is
+  // in. We'll assume here NCHW ?
+  size_t in_size  = in_data.shape()[1];
   size_t out_size = out_data.shape()[1];
 
+  // initialize NNPACK threadpool with maximum number of threads
+  NNPackThreadPool nnp_threadpool;
+  nnp_threadpool.set_max_num_threads();
+  nnp_threadpool.create();
+
   const auto status =
-		nnp_fully_connected_inference(in_size,
-																	out_size,
-																  input_ptr,
-																	kernel_ptr,
-																	output_ptr,
-																	threadpool);
+    nnp_fully_connected_inference(in_size, out_size, input_ptr, kernel_ptr,
+                                  output_ptr, nnp_threadpool.threadpool());
 
   if (status != nnp_status_success) {
     throw nn_error("Could not succeed with nnp_max_pooling_output");
   }
 
-  // TODO: embed it into a class
-  pthreadpool_destroy(threadpool);
+  nnp_threadpool.destroy();
 
-	// apply bias
+  // apply bias
   if (bias.size() > 0) {
     for_i(layer_parallelize, out_size,
           [&](size_t i) { output_ptr[i] += bias.host_at(0, i); });
@@ -71,7 +69,7 @@ inline void fully_connected_op_nnpack(const Tensor<float, S1> &in_data,
   CNN_UNREFERENCED_PARAMETER(bias);
   CNN_UNREFERENCED_PARAMETER(out_data);
   CNN_UNREFERENCED_PARAMETER(layer_parallelize);
-  throw nn_error("TinyDNN has not been compiled with NNPACK support.");
+  throw nn_error("tiny-dnn has not been compiled with NNPACK support.");
 #endif
 }
 
@@ -91,10 +89,7 @@ inline void fully_connected_op_nnpack(const Tensor<double, S1> &in_data,
                                       Tensor<double, S4> &out_data,
                                       const bool layer_parallelize) {
   // fallback to tiny-backend when float_t is double
-  fully_connected_op_internal(in_data,
-															weights,
-															bias,
-														  out_data,
+  fully_connected_op_internal(in_data, weights, bias, out_data,
                               layer_parallelize);
 }
 }  // namespace kernels
--- tiny_dnn/core/kernels/maxpool_op_nnpack.h
+++ tiny_dnn/core/kernels/maxpool_op_nnpack.h
@@ -7,8 +7,6 @@
 */
 #pragma once
 
-#include<thread>
-
 #include "tiny_dnn/core/backend.h"
 #include "tiny_dnn/core/params/maxpool_params.h"
 
@@ -23,54 +21,48 @@ inline void maxpool_op_nnpack(const Tensor<float, S1> &in_data,
   // call singleton to initialize NNPACK
   NNPackInitializer::getInstance().initialize();
 
-	// TODO(edgarriba): recheck after paramters refactor
+  // TODO(edgarriba): recheck after paramters refactor
   const size_t input_channels = params.in.depth_;
 
-	// TODO(edgarriba): recheck after paramters refactor
+  // TODO(edgarriba): recheck after paramters refactor
   const nnp_size input_size = {params.in.width_, params.in.height_};
 
   // input is already padded, so no need to do padding.
   // we'll assume that padding is symmetric
-	const nnp_padding padding = {0.0, 0.0, 0.0, 0.0};
+  const nnp_padding padding = {0.0, 0.0, 0.0, 0.0};
 
-	// TODO(edgarriba): we should get this from maxpool_layer_params
+  // TODO(edgarriba): we should get this from maxpool_layer_params
   const nnp_size pooling_size = {params.pool_size_x, params.pool_size_y};
 
-	// TODO(edgarriba): we should get this from maxpool_layer_params
+  // TODO(edgarriba): we should get this from maxpool_layer_params
   const nnp_size pooling_stride = {params.stride_x, params.stride_y};
 
   const float *input_ptr = in_data.host_pbegin();
   float *output_ptr      = out_data.host_pbegin();
 
-  // TODO: embed it into a class
-  const size_t num_mkl_threads = std::thread::hardware_concurrency();
-  pthreadpool_t threadpool     = pthreadpool_create(num_mkl_threads);
+  // initialize NNPACK threadpool with maximum number of threads
+  NNPackThreadPool nnp_threadpool;
+  nnp_threadpool.set_max_num_threads();
+  nnp_threadpool.create();
 
-	// TODO(edgarriba): we should get this from tensor shape. Ideally N from NCHW.
+  // TODO(edgarriba): we should get this from tensor shape. Ideally N from NCHW.
   const size_t batch_size = 1;
 
-  const auto status =
-		nnp_max_pooling_output(batch_size,
-													 input_channels,
-													 input_size,
-													 padding,
-													 pooling_size,
-													 pooling_stride,
-													 input_ptr,
-													 output_ptr,
-													 threadpool);
+  const auto status = nnp_max_pooling_output(
+    batch_size, input_channels, input_size, padding, pooling_size,
+    pooling_stride, input_ptr, output_ptr, nnp_threadpool.threadpool());
 
   if (status != nnp_status_success) {
     throw nn_error("Could not succeed with nnp_max_pooling_output");
   }
 
-  // TODO: embed it into a class
-  pthreadpool_destroy(threadpool);
+  nnp_threadpool.destroy();
+
 #else
   CNN_UNREFERENCED_PARAMETER(in_data);
   CNN_UNREFERENCED_PARAMETER(out_data);
   CNN_UNREFERENCED_PARAMETER(params);
-  throw nn_error("TinyDNN has not been compiled with NNPACK support.");
+  throw nn_error("tiny-dnn has not been compiled with NNPACK support.");
 #endif
 }
 
