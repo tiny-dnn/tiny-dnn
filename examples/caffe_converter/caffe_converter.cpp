c9b0c949edb97bc4fde92d31b84036d261623dac
EdgAR <edgar.riba@gmail.com>
2017-07-10 22:41:14 -0700

    cpplint caffe_converter.h

--- examples/caffe_converter/caffe_converter.cpp
+++ examples/caffe_converter/caffe_converter.cpp
@@ -5,67 +5,69 @@
     Use of this source code is governed by a BSD-style license that can be found
     in the LICENSE file.
 */
+
 #include <ctime>
 #include <iostream>
 #include <memory>
+
 #define NO_STRICT
 #define CNN_USE_CAFFE_CONVERTER
 #define DNN_USE_IMAGE_API
+
 #include "tiny_dnn/tiny_dnn.h"
 
-using namespace tiny_dnn;
-using namespace tiny_dnn::activation;
-using namespace std;
-
-image<float> compute_mean(const string &mean_file, int width, int height) {
+tiny_dnn::image<float> compute_mean(const std::string &mean_file,
+                                    int width,
+                                    int height) {
   caffe::BlobProto blob;
   detail::read_proto_from_binary(mean_file, &blob);
 
   auto data = blob.mutable_data()->mutable_data();
 
-  image<float> original(data, blob.width(), blob.height(), image_type::bgr);
+  tiny_dnn::image<float> original(data, blob.width(), blob.height(),
+                                  image_type::bgr);
 
   return mean_image(original);
 }
 
-void preprocess(const image<float> &img,
-                const image<float> &mean,
+void preprocess(const tiny_dnn::image<float> &img,
+                const tiny_dnn::image<float> &mean,
                 int width,
                 int height,
-                vec_t *dst) {
-  image<float> resized = resize_image(img, width, height);
+                tiny_dnn::vec_t *dst) {
+  tiny_dnn::image<float> resized = resize_image(img, width, height);
 
-  image<> resized_uint8(resized);
+  tiny_dnn::image<> resized_uint8(resized);
 
   if (!mean.empty()) {
-    image<float> normalized = subtract_scalar(resized, mean);
-    *dst                    = normalized.to_vec();
+    tiny_dnn::image<float> normalized = subtract_scalar(resized, mean);
+    *dst                              = normalized.to_vec();
   } else {
     *dst = resized.to_vec();
   }
 }
 
-vector<string> get_label_list(const string &label_file) {
-  string line;
-  ifstream ifs(label_file.c_str());
+std::vector<std::string> get_label_list(const std::string &label_file) {
+  std::string line;
+  std::ifstream ifs(label_file.c_str());
 
   if (ifs.fail() || ifs.bad())
     throw runtime_error("failed to open:" + label_file);
 
-  vector<string> lines;
+  std::vector<std::string> lines;
   while (getline(ifs, line)) lines.push_back(line);
 
   return lines;
 }
 
-void test(const string &model_file,
-          const string &trained_file,
-          const string &mean_file,
-          const string &label_file,
-          const string &img_file) {
+void test(const std::string &model_file,
+          const std::string &trained_file,
+          const std::string &mean_file,
+          const std::string &label_file,
+          const std::string &img_file) {
   auto labels = get_label_list(label_file);
-  auto net    = create_net_from_caffe_prototxt(model_file);
-  reload_weight_from_caffe_protobinary(trained_file, net.get());
+  auto net    = tiny_dnn::create_net_from_caffe_prototxt(model_file);
+  tiny_dnn::reload_weight_from_caffe_protobinary(trained_file, net.get());
 
   // int channels = (*net)[0]->in_data_shape()[0].depth_;
   int width  = (*net)[0]->in_data_shape()[0].width_;
@@ -73,9 +75,9 @@ void test(const string &model_file,
 
   auto mean = compute_mean(mean_file, width, height);
 
-  image<float> img(img_file, image_type::bgr);
+  tiny_dnn::image<float> img(img_file, image_type::bgr);
 
-  vec_t vec;
+  tiny_dnn::vec_t vec;
 
   preprocess(img, mean, width, height, &vec);
 
@@ -87,7 +89,7 @@ void test(const string &model_file,
   double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
   cout << "Elapsed time(s): " << elapsed_secs << endl;
 
-  vector<tiny_dnn::float_t> sorted(result.begin(), result.end());
+  std::vector<tiny_dnn::float_t> sorted(result.begin(), result.end());
 
   int top_n = 5;
   partial_sort(sorted.begin(), sorted.begin() + top_n, sorted.end(),
@@ -101,12 +103,12 @@ void test(const string &model_file,
 }
 
 int main(int argc, char **argv) {
-  int arg_channel     = 1;
-  string model_file   = argv[arg_channel++];
-  string trained_file = argv[arg_channel++];
-  string mean_file    = argv[arg_channel++];
-  string label_file   = argv[arg_channel++];
-  string img_file     = argv[arg_channel++];
+  int arg_channel          = 1;
+  std::string model_file   = argv[arg_channel++];
+  std::string trained_file = argv[arg_channel++];
+  std::string mean_file    = argv[arg_channel++];
+  std::string label_file   = argv[arg_channel++];
+  std::string img_file     = argv[arg_channel++];
 
   try {
     test(model_file, trained_file, mean_file, label_file, img_file);
