2040b1a12e824fdb123926626b5d83290d387d42
Lucas <lucaspbordignon99@gmail.com>
2017-07-17 15:25:04 -0300

    nesterov_momentum test

--- test/test.cpp
+++ test/test.cpp
@@ -40,6 +40,7 @@ using namespace tiny_dnn::activation;
 #include "test_slice_layer.h"
 #include "test_target_cost.h"
 #include "test_tensor.h"
+#include "test_optimizers.h"
 
 #ifndef CNN_NO_SERIALIZATION
 #include "test_serialization.h"
