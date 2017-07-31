0d4878f7cf0fe8b4093ae447958360add38a9647
Evgenii <eabesea@rambler.ru>
2017-07-31 15:12:08 +0300

    Global avepool AVX Tensor integration

--- tiny_dnn/core/kernels/global_avepool_op_avx.h
+++ tiny_dnn/core/kernels/global_avepool_op_avx.h
@@ -14,13 +14,15 @@ namespace kernels {
 #ifdef CNN_USE_DOUBLE
 
 // double version
-inline void global_avepool_op_avx(const tensor_t &in_data,
-                                  tensor_t &out_data,
+template <typename S1, typename S2>
+inline void global_avepool_op_avx(const Tensor<double, S1> &in_data,
+                                  Tensor<double, S1> &out_data,
                                   const core::global_avepool_params &params,
                                   const bool layer_parallelize) {
   const size_t pool_area            = params.in.width_ * params.in.height_;
   const size_t nblocks_per_channel  = pool_area / 4;
   const size_t nremains_per_channel = pool_area & 3;
+  const size_t sample_num           = in_data.shape()[0];
 
   static const int64_t mask_src[] = {-1, -1, -1, -1, 0, 0, 0, 0};
   __m256i imask =
@@ -29,9 +31,9 @@ inline void global_avepool_op_avx(const tensor_t &in_data,
   __m128d pool_area_m     = _mm_cvtsi64_sd(_mm_setzero_pd(), pool_area);
   __m128d pool_area_inv_m = _mm_div_sd(one_m, pool_area_m);
 
-  for_i(layer_parallelize, in_data.size(), [&](size_t sample) {
-    const vec_t &in = in_data[sample];
-    vec_t &out      = out_data[sample];
+  for_i(layer_parallelize, sample_num, [&](size_t sample) {
+    const auto in = in_data.host_pointer(sample, 0);
+    auto out      = out_data.host_pointer(sample, 0);
     for (size_t i = 0; i < params.in.depth_; i++) {
       const double *pin = &in[i * pool_area];
       __m256d sum0 =
@@ -45,24 +47,26 @@ inline void global_avepool_op_avx(const tensor_t &in_data,
 }
 
 // double version
+template <typename S1, typename S2>
 inline void global_avepool_grad_op_avx(
-  tensor_t &prev_delta,
-  const tensor_t &curr_delta,
+  Tensor<double, S1> &prev_delta,
+  const Tensor<double, S2> &curr_delta,
   const core::global_avepool_params &params,
   const bool layer_parallelize) {
   const size_t pool_area            = params.in.width_ * params.in.height_;
   const double pool_area_inv        = 1.0 / pool_area;
   const size_t nblocks_per_channel  = pool_area / 4;
   const size_t nremains_per_channel = pool_area & 3;
+  const size_t sample_num           = prev_delta.shape()[0];
 
   int64_t mask_src[] = {-1, -1, -1, -1, 0, 0, 0, 0};
   __m256i imask =
     _mm256_loadu_si256((__m256i const *)(mask_src + 4 - nremains_per_channel));
   __m256d pool_area_inv_m = _mm256_set1_pd(pool_area_inv);
 
-  for_i(layer_parallelize, prev_delta.size(), [&](size_t sample) {
-    vec_t &prev       = prev_delta[sample];
-    const vec_t &curr = curr_delta[sample];
+  for_i(layer_parallelize, sample_num, [&](size_t sample) {
+    auto in        = prev_delta.host_pointer(sample, 0);
+    const auto out = curr_delta.host_pointer(sample, 0);
 
     for (size_t i = 0; i < params.in.depth_; i++) {
       size_t j       = 0;
@@ -71,7 +75,7 @@ inline void global_avepool_grad_op_avx(
 
       while (j < nblocks_per_channel) {
         _mm256_storeu_pd(&prev[i * pool_area + 4 * j], prev_m);
-        j++;
+        ++j;
       }
       _mm256_maskstore_pd(&prev[i * pool_area + 4 * j], imask, prev_m);
     }
@@ -81,13 +85,15 @@ inline void global_avepool_grad_op_avx(
 #else  // CNN_USE_DOUBLE
 
 // float version
-inline void global_avepool_op_avx(const tensor_t &in_data,
-                                  tensor_t &out_data,
+template <typename S1, typename S2>
+inline void global_avepool_op_avx(const Tensor<float, S1> &in_data,
+                                  Tensor<float, S2> &out_data,
                                   const core::global_avepool_params &params,
                                   const bool layer_parallelize) {
   const size_t pool_area            = params.in.width_ * params.in.height_;
   const size_t nblocks_per_channel  = pool_area / 8;
   const size_t nremains_per_channel = pool_area & 7;
+  const size_t sample_num           = in_data.shape()[0];
 
   static const int32_t mask_src[] = {
     -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -99,9 +105,10 @@ inline void global_avepool_op_avx(const tensor_t &in_data,
     _mm_cvtsi32_ss(_mm_setzero_ps(), static_cast<int>(pool_area));
   __m128 pool_area_inv_m = _mm_div_ss(one_m, pool_area_m);
 
-  for_i(layer_parallelize, in_data.size(), [&](size_t sample) {
-    const vec_t &in = in_data[sample];
-    vec_t &out      = out_data[sample];
+  for_i(layer_parallelize, sample_num, [&](size_t sample) {
+    const auto in = in_data.host_pointer(sample, 0);
+    auto out      = out_data.host_pointer(sample, 0);
+
     for (size_t i = 0; i < params.in.depth_; i++) {
       const float *pin = &in[i * pool_area];
       __m256 sum0 =
@@ -115,15 +122,17 @@ inline void global_avepool_op_avx(const tensor_t &in_data,
 }
 
 // float version
+template <typename S1, typename S2>
 inline void global_avepool_grad_op_avx(
-  tensor_t &prev_delta,
-  const tensor_t &curr_delta,
+  Tensor<float, S1> &prev_delta,
+  const Tensor<float, S2> &curr_delta,
   const core::global_avepool_params &params,
   const bool layer_parallelize) {
   const size_t pool_area            = params.in.width_ * params.in.height_;
   const float pool_area_inv         = 1.0f / static_cast<float>(pool_area);
   const size_t nblocks_per_channel  = pool_area / 8;
   const size_t nremains_per_channel = pool_area & 7;
+  const size_t sample_num           = prev_delta.shape()[0];
 
   int32_t mask_src[] = {
     -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -131,17 +140,19 @@ inline void global_avepool_grad_op_avx(
   __m256i imask =
     _mm256_loadu_si256((__m256i const *)(mask_src + 8 - nremains_per_channel));
 
-  for_i(layer_parallelize, prev_delta.size(), [&](size_t sample) {
-    vec_t &prev       = prev_delta[sample];
-    const vec_t &curr = curr_delta[sample];
+  for_i(layer_parallelize, sample_num, [&](size_t sample) {
+    auto prev       = prev_delta.host_pointer(sample, 0);
+    const auto curr = curr_delta.host_pointer(sample, 0);
+    ;
 
     for (size_t i = 0; i < params.in.depth_; i++) {
-      size_t j            = 0;
+      size_t j = 0;
+
       const __m256 prev_m = _mm256_set1_ps(curr[i] * pool_area_inv);
 
       while (j < nblocks_per_channel) {
         _mm256_storeu_ps(&prev[i * pool_area + 8 * j], prev_m);
-        j++;
+        ++j;
       }
       _mm256_maskstore_ps(&prev[i * pool_area + 8 * j], imask, prev_m);
     }
