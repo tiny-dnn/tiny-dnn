079e2d3149d8ae7a8c1d8b434d48934f79d8889f
Karan Desai <karandesai_96@live.com>
2017-08-27 13:29:20 +0530

    Fix optimizer tests to work on clang.

--- test/test_optimizers.h
+++ test/test_optimizers.h
@@ -15,13 +15,13 @@ namespace tiny_dnn {
 TEST(optimizers, adagrad_update) {
   adagrad optimizer;
 
-  Tensor<> weights({0.20, 0.40, 0.006, -0.77, -0.010});
-  Tensor<> gradients({1.00, -3.24, -0.600, 2.79, 1.820});
+  Tensor<> weights{vec_t{0.20, 0.40, 0.006, -0.77, -0.010}};
+  Tensor<> gradients{vec_t{1.00, -3.24, -0.600, 2.79, 1.820}};
 
   // Defining the expected updates
 
-  Tensor<> first_update({0.1900, 0.4100, 0.0160, -0.7800, -0.0200});
-  Tensor<> second_update({0.1829, 0.4170, 0.0230, -0.7870, -0.0270});
+  Tensor<> first_update{vec_t{0.1900, 0.4100, 0.0160, -0.7800, -0.0200}};
+  Tensor<> second_update{vec_t{0.1829, 0.4170, 0.0230, -0.7870, -0.0270}};
 
   // Testing
   optimizer.update(gradients, weights, false);
@@ -40,13 +40,13 @@ TEST(optimizers, adagrad_update) {
 TEST(optimizers, rmsprop_update) {
   RMSprop optimizer;
 
-  Tensor<> weights({-0.021, 1.03, -0.05, -.749, 0.009});
-  Tensor<> gradients({1.000, -3.24, -0.60, 2.79, 1.820});
+  Tensor<> weights{vec_t{-0.021, 1.03, -0.05, -.749, 0.009}};
+  Tensor<> gradients{vec_t{1.000, -3.24, -0.60, 2.79, 1.820}};
 
   // Defining the expected updates
 
-  Tensor<> first_update({-0.0220, 1.0310, -0.0490, -0.7500, 0.0080});
-  Tensor<> second_update({-0.0227, 1.0317, -0.0482, -0.7507, 0.0072});
+  Tensor<> first_update{vec_t{-0.0220, 1.0310, -0.0490, -0.7500, 0.0080}};
+  Tensor<> second_update{vec_t{-0.0227, 1.0317, -0.0482, -0.7507, 0.0072}};
 
   // Testing
   optimizer.update(gradients, weights, false);
@@ -65,12 +65,12 @@ TEST(optimizers, rmsprop_update) {
 TEST(optimizers, adam_update) {
   adam optimizer;
 
-  Tensor<> weights({1.00, 0.081, -0.6201, 0.96, -0.007});
-  Tensor<> gradients({6.45, -3.240, -0.6000, 2.79, 1.820});
+  Tensor<> weights{vec_t{1.00, 0.081, -0.6201, 0.96, -0.007}};
+  Tensor<> gradients{vec_t{6.45, -3.240, -0.6000, 2.79, 1.820}};
 
   // Defining the expected updates
-  Tensor<> first_update({0.9992, 0.0817, -0.6193, 0.9592, -0.0077});
-  Tensor<> second_update({0.9983, 0.0826, -0.6184, 0.9583, -0.0086});
+  Tensor<> first_update{vec_t{0.9992, 0.0817, -0.6193, 0.9592, -0.0077}};
+  Tensor<> second_update{vec_t{0.9983, 0.0826, -0.6184, 0.9583, -0.0086}};
 
   // Testing
   optimizer.update(gradients, weights, false);
@@ -89,13 +89,13 @@ TEST(optimizers, adam_update) {
 TEST(optimizers, adamax_update) {
   adamax optimizer;
 
-  Tensor<> weights({1.00, 0.081, -0.6201, 0.96, -0.007});
-  Tensor<> gradients({6.45, -3.240, -0.6000, 2.79, 1.820});
+  Tensor<> weights{vec_t{1.00, 0.081, -0.6201, 0.96, -0.007}};
+  Tensor<> gradients{vec_t{6.45, -3.240, -0.6000, 2.79, 1.820}};
 
   // Defining the expected updates
 
-  Tensor<> first_update({0.9980, 0.0830, -0.6181, 0.9580, -0.0090});
-  Tensor<> second_update({0.9960, 0.0850, -0.6161, 0.9560, -0.0109});
+  Tensor<> first_update{vec_t{0.9980, 0.0830, -0.6181, 0.9580, -0.0090}};
+  Tensor<> second_update{vec_t{0.9960, 0.0850, -0.6161, 0.9560, -0.0109}};
 
   // Testing
   optimizer.update(gradients, weights, false);
@@ -114,13 +114,13 @@ TEST(optimizers, adamax_update) {
 TEST(optimizers, naive_sgd_update) {
   gradient_descent optimizer;
 
-  Tensor<> weights({-0.001, -0.90, 0.005, -0.74, 0.003});
-  Tensor<> gradients({-2.240, -3.24, 0.600, 0.39, 0.820});
+  Tensor<> weights{vec_t{-0.001, -0.90, 0.005, -0.74, 0.003}};
+  Tensor<> gradients{vec_t{-2.240, -3.24, 0.600, 0.39, 0.820}};
 
   // Defining the expected updates
 
-  Tensor<> first_update({0.0214, -0.8676, -0.0010, -0.7439, -0.0052});
-  Tensor<> second_update({0.0438, -0.8352, -0.0070, -0.7478, -0.0134});
+  Tensor<> first_update{vec_t{0.0214, -0.8676, -0.0010, -0.7439, -0.0052}};
+  Tensor<> second_update{vec_t{0.0438, -0.8352, -0.0070, -0.7478, -0.0134}};
 
   // Testing
   optimizer.update(gradients, weights, false);
@@ -139,13 +139,13 @@ TEST(optimizers, naive_sgd_update) {
 TEST(optimizers, momentum_update) {
   momentum optimizer;
 
-  Tensor<> weights({-0.001, -0.90, 0.005, -0.74, 0.003});
-  Tensor<> gradients({-2.240, -3.24, 0.600, 0.39, 0.820});
+  Tensor<> weights{vec_t{-0.001, -0.90, 0.005, -0.74, 0.003}};
+  Tensor<> gradients{vec_t{-2.240, -3.24, 0.600, 0.39, 0.820}};
 
   // Defining the expected updates
 
-  Tensor<> first_update({0.0214, -0.8676, -0.0010, -0.7439, -0.0052});
-  Tensor<> second_update({0.0639, -0.8060, -0.0124, -0.7513, -0.0207});
+  Tensor<> first_update{vec_t{0.0214, -0.8676, -0.0010, -0.7439, -0.0052}};
+  Tensor<> second_update{vec_t{0.0639, -0.8060, -0.0124, -0.7513, -0.0207}};
 
   // Testing
   optimizer.update(gradients, weights, false);
@@ -164,12 +164,12 @@ TEST(optimizers, momentum_update) {
 TEST(optimizers, nesterov_momentum_update) {
   nesterov_momentum optimizer;
 
-  Tensor<> weights({0.1, 0.30, 0.005, -0.74, -0.008});
-  Tensor<> gradients({1.0, -3.24, -0.600, 2.79, 1.820});
+  Tensor<> weights{vec_t{0.1, 0.30, 0.005, -0.74, -0.008}};
+  Tensor<> gradients{vec_t{1.0, -3.24, -0.600, 2.79, 1.820}};
 
   // Defining the expected updates
-  Tensor<> first_update({0.0810, 0.3615, 0.0164, -0.7930, -0.0425});
-  Tensor<> second_update({0.0539, 0.4493, 0.0326, -0.8686, -0.0919});
+  Tensor<> first_update{vec_t{0.0810, 0.3615, 0.0164, -0.7930, -0.0425}};
+  Tensor<> second_update{vec_t{0.0539, 0.4493, 0.0326, -0.8686, -0.0919}};
 
   // Testing
   optimizer.update(gradients, weights, false);
