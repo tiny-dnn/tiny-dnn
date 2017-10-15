705eef7eb6a4bc7f405a4659738376f75883dd8d
Lakshay Garg <lakshayg@outlook.in>
2017-10-16 03:04:37 +0530

    add asinh activation function

--- tiny_dnn/tiny_dnn.h
+++ tiny_dnn/tiny_dnn.h
@@ -16,10 +16,23 @@
 #include "tiny_dnn/core/framework/device.h"
 #include "tiny_dnn/core/framework/program_manager.h"
 
+#include "tiny_dnn/activations/asinh_layer.h"
+#include "tiny_dnn/activations/elu_layer.h"
+#include "tiny_dnn/activations/leaky_relu_layer.h"
+#include "tiny_dnn/activations/relu_layer.h"
+#include "tiny_dnn/activations/selu_layer.h"
+#include "tiny_dnn/activations/sigmoid_layer.h"
+#include "tiny_dnn/activations/softmax_layer.h"
+#include "tiny_dnn/activations/softplus_layer.h"
+#include "tiny_dnn/activations/softsign_layer.h"
+#include "tiny_dnn/activations/tanh_layer.h"
+#include "tiny_dnn/activations/tanh_p1m2_layer.h"
 #include "tiny_dnn/layers/arithmetic_layer.h"
 #include "tiny_dnn/layers/average_pooling_layer.h"
 #include "tiny_dnn/layers/average_unpooling_layer.h"
 #include "tiny_dnn/layers/batch_normalization_layer.h"
+#include "tiny_dnn/layers/cell.h"
+#include "tiny_dnn/layers/cells.h"
 #include "tiny_dnn/layers/concat_layer.h"
 #include "tiny_dnn/layers/convolutional_layer.h"
 #include "tiny_dnn/layers/deconvolutional_layer.h"
@@ -36,20 +49,6 @@
 #include "tiny_dnn/layers/quantized_deconvolutional_layer.h"
 #include "tiny_dnn/layers/recurrent_layer.h"
 #include "tiny_dnn/layers/slice_layer.h"
-
-#include "tiny_dnn/layers/cell.h"
-#include "tiny_dnn/layers/cells.h"
-
-#include "tiny_dnn/activations/elu_layer.h"
-#include "tiny_dnn/activations/leaky_relu_layer.h"
-#include "tiny_dnn/activations/relu_layer.h"
-#include "tiny_dnn/activations/selu_layer.h"
-#include "tiny_dnn/activations/sigmoid_layer.h"
-#include "tiny_dnn/activations/softmax_layer.h"
-#include "tiny_dnn/activations/softplus_layer.h"
-#include "tiny_dnn/activations/softsign_layer.h"
-#include "tiny_dnn/activations/tanh_layer.h"
-#include "tiny_dnn/activations/tanh_p1m2_layer.h"
 
 #ifdef CNN_USE_GEMMLOWP
 #include "tiny_dnn/layers/quantized_fully_connected_layer.h"
@@ -132,6 +131,8 @@ using ave_unpool = tiny_dnn::average_unpooling_layer;
 namespace activation {
 
 using sigmoid = tiny_dnn::sigmoid_layer;
+
+using asinh = tiny_dnn::asinh_layer;
 
 using tanh = tiny_dnn::tanh_layer;
 
