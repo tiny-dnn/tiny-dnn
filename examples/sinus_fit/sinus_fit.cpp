e6b3d0996db3e252f84cf0e3345a48cdb66565c3
edeforas <etienne.deforas@gmail.com>
2018-03-11 18:34:07 +0100

    changed author to use common headers

--- examples/sinus_fit/sinus_fit.cpp
+++ examples/sinus_fit/sinus_fit.cpp
@@ -1,7 +1,6 @@
 /*
-    Copyright (c) 2018, Etienne de Foras and the respective contributors
+    Copyright (c) 2013, Taiga Nomi and the respective contributors
     All rights reserved.
-
     Use of this source code is governed by a BSD-style license that can be found
     in the LICENSE file.
 */
