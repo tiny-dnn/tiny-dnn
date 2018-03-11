72e7591b4651520d1a74ef0f9dfc2dad1b78766e
edeforas <etienne.deforas@gmail.com>
2018-03-11 17:22:18 +0100

    fixes clang-format and cpplint

--- examples/sinus_fit/sinus_fit.cpp
+++ examples/sinus_fit/sinus_fit.cpp
@@ -1,3 +1,11 @@
+/*
+    Copyright (c) 2018, Etienne de Foras and the respective contributors
+    All rights reserved.
+
+    Use of this source code is governed by a BSD-style license that can be found
+    in the LICENSE file.
+*/
+
 // this example show how to use tiny-dnn library to fit data, by learning a
 // sinus function
 
@@ -5,27 +13,25 @@
 // https://github.com/tiny-dnn/tiny-dnn/blob/master/docs/how_tos/How-Tos.md
 
 #include <iostream>
-using namespace std;
 
 #include "tiny_dnn/tiny_dnn.h"
-using namespace tiny_dnn;
 
 int main() {
   // create a simple network with 2 layer of 10 neurons each
   // input is x, output is sin(x)
-  network<sequential> net;
-  net << fully_connected_layer(1, 10);
-  net << tanh_layer();
-  net << fully_connected_layer(10, 10);
-  net << tanh_layer();
-  net << fully_connected_layer(10, 1);
+  tiny_dnn::network<tiny_dnn::sequential> net;
+  net << tiny_dnn::fully_connected_layer(1, 10);
+  net << tiny_dnn::tanh_layer();
+  net << tiny_dnn::fully_connected_layer(10, 10);
+  net << tiny_dnn::tanh_layer();
+  net << tiny_dnn::fully_connected_layer(10, 1);
 
   // create input and desired output on a period
-  vector<vec_t> X;
-  vector<vec_t> sinusX;
+  std::vector<tiny_dnn::vec_t> X;
+  std::vector<tiny_dnn::vec_t> sinusX;
   for (float x = -3.1416f; x < 3.1416f; x += 0.2f) {
-    vec_t vx    = {x};
-    vec_t vsinx = {sinf(x)};
+    tiny_dnn::vec_t vx    = {x};
+    tiny_dnn::vec_t vsinx = {sinf(x)};
 
     X.push_back(vx);
     sinusX.push_back(vsinx);
@@ -34,7 +40,7 @@ int main() {
   // set learning parameters
   size_t batch_size = 16;    // 16 samples for each network weight update
   int epochs        = 2000;  // 2000 presentation of all samples
-  adamax opt;
+  tiny_dnn::adamax opt;
 
   // this lambda function will be called after each epoch
   int iEpoch              = 0;
@@ -43,31 +49,37 @@ int main() {
     iEpoch++;
     if (iEpoch % 100) return;
 
-    double loss = net.get_loss<mse>(X, sinusX);
-    cout << "epoch=" << iEpoch << "/" << epochs << " loss=" << loss << endl;
+    double loss = net.get_loss<tiny_dnn::mse>(X, sinusX);
+    std::cout << "epoch=" << iEpoch << "/" << epochs << " loss=" << loss
+              << std::endl;
   };
 
   // learn
-  cout << "learning the sinus function with 2000 epochs:" << endl;
-  net.fit<mse>(opt, X, sinusX, batch_size, epochs, []() {}, on_enumerate_epoch);
+  std::cout << "learning the sinus function with 2000 epochs:" << std::endl;
+  net.fit<tiny_dnn::mse>(opt, X, sinusX, batch_size, epochs, []() {},
+                         on_enumerate_epoch);
+
+  std::cout << std::endl
+            << "Training finished, now computing prediction results:"
+            << std::endl;
 
   // compare prediction and desired output
-  float dMaxError = 0.f;
+  float fMaxError = 0.f;
   for (float x = -3.1416f; x < 3.1416f; x += 0.2f) {
-    vec_t xv         = {x};
-    float fPredicted = net.predict(xv)[0];
-    float fDesired   = sinf(x);
+    tiny_dnn::vec_t xv = {x};
+    float fPredicted   = net.predict(xv)[0];
+    float fDesired     = sinf(x);
 
-    cout << "x=" << x << " sinX=" << fDesired << " predicted=" << fPredicted
-         << endl;
+    std::cout << "x=" << x << " sinX=" << fDesired
+              << " predicted=" << fPredicted << std::endl;
 
     // update max error
-    float dError = abs(fPredicted - fDesired);
+    float fError = fabs(fPredicted - fDesired);
 
-    if (dMaxError < dError) dMaxError = dError;
+    if (fMaxError < fError) fMaxError = fError;
   }
 
-  cout << endl << "max_error=" << dMaxError << endl;
+  std::cout << std::endl << "max_error=" << fMaxError << std::endl;
 
   return 0;
 }
